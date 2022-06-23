9816024d89479ccfcc0f858000ca4ca383ab604e
Susheel Kumar <susheel.kumar.msl@gmail.com>
2022-06-23 21:30:23 +0530

    Bind Reducers

---
+++ index.js
@@ -0,0 +1,62 @@
+const redux               = require('redux');
+const createStore         = redux.createStore
+const bindActionCreators  = redux.bindActionCreators
+
+
+const CAKE_ORDERED        = 'CAKE_ORDERED';
+const CAKE_RESTOCKED      = 'CAKE_RESTOCKED';
+
+function orderCake(){
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  }
+}
+
+function restockCake(qty = 1){
+  return{
+    type:CAKE_RESTOCKED,
+    payload: qty,
+  }
+} 
+
+const initialState = {
+  numOfCakes: 10,
+}
+
+// reducer (previousState, action) => newState
+const reducer = (state = initialState, action) => {
+  switch(action.type){
+    case CAKE_ORDERED:
+      return {
+        numOfCakes: state.numOfCakes - 1
+      }
+    case CAKE_RESTOCKED:
+      return{
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload
+      }  
+    default:
+      return state    
+  }
+}
+
+const store = createStore(reducer);
+  console.log('Initial State', store.getState())
+
+const unsubscribe = store.subscribe(() => 
+  console.log('Updated State', store.getState())
+);
+
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(3))
+
+const actions = bindActionCreators({orderCake, restockCake}, store.dispatch);
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
+
+unsubscribe()
